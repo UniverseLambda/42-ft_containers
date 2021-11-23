4761a63f1eb10da68f77bb939b325f7d0eef4a96
SAAD Clement <44324085+UniverseLambda@users.noreply.github.com>
2021-11-23 18:13:00 +0100

    vector: add constructors

--- Vector.hpp
+++ Vector.hpp
@@ -22,5 +22,62 @@ namespace ft {
 		// FIXME: Merge reverse_iterator changes
 		// typedef _Allocator reverse_iterator;
 		// typedef _Allocator const_reverse_iterator;
+
+	private:
+		allocator_type mAlloc;
+		pointer mData;
+		size_type mSize;
+		size_type mCapacity;
+
+	public:
+		vector():
+			mAlloc(),
+			mData(mAlloc.allocate(0)),
+			mSize(0),
+			mCapacity(0) {}
+
+		explicit vector(const allocator_type &alloc):
+			mAlloc(alloc),
+			mData(mAlloc.allocate(0)),
+			mSize(0),
+			mCapacity(0) {}
+
+		explicit vector(size_type count, const value_type &value = value_type(), const allocator_type &alloc = allocator_type()):
+			mAlloc(alloc),
+			mData(mAlloc.allocate(count)),
+			mSize(count),
+			mCapacity(count) {
+			for (size_type i = 0; i < count; ++i) {
+				mAlloc.construct(&(mData[i]), value);
+			}
+		}
+
+		explicit vector(size_type count):
+			mAlloc(),
+			mData(mAlloc.allocate(count)),
+			mSize(count),
+			mCapacity(count) {
+			for (size_type i = 0; i < count; ++i) {
+				mAlloc.construct(&(mData[i]));
+			}
+		}
+
+		// template<typename _InputIt>
+		// vector(_InputIt first, _InputIt last, const allocator_type &alloc = allocator_type()):
+		// 	mAlloc(alloc), mData(mAlloc.allocate(0)), mSize(0), mCapacity(0) {
+		// 	for (; first != last; ++first) {
+		// 		push_back(*first);
+		// 	}
+		// }
+
+		vector(const vector &other): mAlloc(other.mAlloc) {
+			mData = mAlloc(other.mCapacity);
+			mSize = other.mSize;
+			mCapacity = other.mCapacity;
+
+			for (size_type i = 0; i < mSize; ++i) {
+				mAlloc.construct(&(mData[i]), other.mData[i]);
+			}
+		}
 	};
 } // namespace ft
--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 #include "Vector.hpp"
 
 int main(void) {
-	__clsaad_impl::PtrIterator<int, ft::vector<int> > it;
+	ft::__clsaad_impl::PtrIterator<int, ft::vector<int> > it;
 
-	ft::iterator_traits<__clsaad_impl::PtrIterator<int, ft::vector<int> > >::value_type i;
+	ft::iterator_traits<ft::__clsaad_impl::PtrIterator<int, ft::vector<int> > >::value_type i;
 
 	i = 50;
 
